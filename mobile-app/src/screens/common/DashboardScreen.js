b8c0bdfad9197324c00a1c61dc367c72a5caa015
Senal Anuraheesaera <senalanuraheesara@gmail.com>
2026-04-04 00:28:18 +0530

    added file

--- mobile-app/src/screens/common/DashboardScreen.js
+++ mobile-app/src/screens/common/DashboardScreen.js
@@ -214,4 +214,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: SPACING.sm,
   },
+  menuLabel: {
+    fontSize: 14,
+    fontWeight: FONTS.semiBold,
+    color: COLORS.text,
+    textAlign: 'center',
+  },
 });
